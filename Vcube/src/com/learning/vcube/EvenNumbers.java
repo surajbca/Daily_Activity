7e6162a63de63a8cfbf925e00809004a9d264bea
krsur <krsur@Suraj>
2026-08-10 10:57:41 +0530

    Create EvenNumbers class and sumNumbers class

---
+++ Vcube/src/com/learning/vcube/EvenNumbers.java
@@ -0,0 +1,13 @@
+package com.learning.vcube;
+
+public class EvenNumbers {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 100; i++) {
+			if(i % 2== 0) {
+				System.out.println("Even number " + i );			}
+		}
+	}
+
+}
